cb888b34ab000eeb7cc60efe4f34e151b0fba6d5
juraj.palenik@uib.no <Anomander47>
2022-01-24 09:41:17 +0100

    Some more changes

--- src/glidar_model/convection_model.py
+++ src/glidar_model/convection_model.py
@@ -12,21 +12,6 @@ from scipy import interpolate
 from glidar_model.moist_cache import MoistCache
 
 
-class Utils:
-
-    @staticmethod
-    def combine_pint_arrays(a, b, idx):
-
-        if not a.check(b.units):
-            raise ValueError('Incompatible units', a, b)
-
-        b_star = b.to(a.units)
-        A = a.magnitude
-        B = b_star.magnitude
-        A[idx:] = B[idx:]
-        return A * a.units
-
-
 class AirProfile:
     """
     This is the class containing sounding data
